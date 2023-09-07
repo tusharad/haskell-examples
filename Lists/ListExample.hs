69607c484fdbaf5cfc7a2a8374194f5aee0a70dd
tusharad <tusharadhatrao@gmail.com>
2023-09-07 15:06:18 +0530

    added List operations

--- Lists/ListExample.hs
+++ Lists/ListExample.hs
@@ -16,11 +16,34 @@ doubleAll (x:xs) = (x+x) : doubleAll xs
 squareAll :: [Int] -> [Int]
 squareAll list1 = map (\x -> x*x) list1
 
+printList :: Show a => [a] -> IO ()
+printList [] = return ()
+printList (x:xs) = do
+  print x
+  printList xs
+
+
+{-
+This is a standard way of writing helper function (functionName_). Sometimes while using recursion, we often need more arguments;
+maybe to store result. We can't expects these arguments to come from end user. Hence, we write helper function to pass extra arguments.
+-}
+mergeTwoSortedLists_ :: Ord a => [a] -> [a] -> [a] -> [a]
+mergeTwoSortedLists_ [] ys res = res ++ ys
+mergeTwoSortedLists_ xs [] res = res ++ xs
+mergeTwoSortedLists_ (x:xs) (y:ys) res 
+  | x < y = mergeTwoSortedLists_ xs (y:ys) (res ++ [x])
+  | x > y = mergeTwoSortedLists_ (x:xs) ys (res ++ [y])
+  | otherwise = mergeTwoSortedLists_ xs ys (res ++ [x,y])
+
+mergeTwoSortedLists :: Ord a => [a] -> [a] -> [a]
+mergeTwoSortedLists xs ys = mergeTwoSortedLists_ xs ys []
+
 main :: IO ()
 main = do
   print $ sum [1,2,3]         -- sum of a list
   print $ head [1,2,3]        -- get the first element of the list
   print $ tail [1,2,3]        -- get the list without the first element
+  print $ last [1,2,3]        -- get the last element of the list [link](https://stackoverflow.com/questions/7376937/fastest-way-to-get-the-last-element-of-a-list-in-haskell)
   print $ 5 : [1,2,3]         -- create a new list, in which 5 is added in the front of the list
   print $ [5] ++ [1,2,3]      -- concate two lists
   print $ [1,2,3] ++ [5]      -- concate two lists
@@ -28,3 +51,15 @@ main = do
   print $ doubleAll [1,2,3,4] -- double each element in the list by using recursion and pattern matching
   print $ squareAll [1,2,3,4] -- square each element in the list by using map
   putStrLn $ balancedSums [5,6,8,11] -- print yes if find an element of the array such that the sum of all elements to the left is equal to the sum of all elements to the right
+  printList [1,2,3,4]         -- haskell function to print each element of the list on new line
+  print $ [1,2,3,4] !! 2      -- Get the ith element from haskell list (bang bang sign)
+  -- Slicing of list can be done in 2 ways, using take and drop
+  print $ take 2 [1,2,3,4]    -- Takes first 2 elements of list
+  print $ drop 2 [1,2,3,4]    -- Drop first 2 element and returns remaining list
+  -- print $ delete 2 [1,2,2,3,4]  -- Removes the first occurance of the element from the list, part of Data.List
+  print $ mergeTwoSortedLists [1,4,6] [2,3,5]
+  -- List comprehension 
+  print $ [x*x | x <-  [1,2,3]] -- Squaring each element in the list using list comprehension
+  print $ [(x,y) | x <-  [1,2,3], y <- [4,5,6]] -- Generate all possible combinations of tuple from 2 lists
+  print $ [x | x <-  [1..10], even x] -- Get a list that contains even numbers between 1 to 10
+  print $ [(x,y) | x <-  [1..5], y <- [1..5],(x+y) == 4] -- Generate list of tuples where the sum of both numbers is 4
--- Lists/README.md
+++ Lists/README.md
@@ -1,5 +1,39 @@
 # Lists
 
+Lists are an ordered sequence / collection of data of the same type, List is a very useful data strucutre in haskell. 
+In haskell, Lists are immutable, i.e Whenever we perform any operation such as head, take, we are generating a new list from the
+previous list.
+
+
+## Cons operator
+
+List are generated with the help of cons operator (:).
+E.g [1,2,3] == 1 : 2 : 3 : []
+
+You can think like an element is gettng added at the front of the list with each cons operator.
+Remeber the preceance for the cons operator is like this:
+    (1:(2:(3:[])))
+
+The type of cons operator is:
+```
+a -> [a] -> [a]
+```
+i.e in the infix form, The left should be the element of type a and the right should be the list of a.
+You cannot go like this, 
+[1,2,3] : 4 # This is wrong
+and expect element 4 would be appended at the end.
+If you want to append the element i.e add the element at the end; you should do something like this;
+[1,2,3] ++ [4] -> [1,2,3,4]
+
+## Pattern matching
+
+You can see, I have used pattern-matching. Basically it matches the given arguments with our function arguments; If the argument is same, then the following operation would be done,
+similar to switch case;
+The sequence is pretty important; Haskell will match it with first case firstl so make sure to put specific cases at the beginning and default ones at the end.
+In Recursion, we can pattern match with base case.
+
+## List comprehension
+
 In the file, I will be covering...
 - Basic list operations
 - Maps
